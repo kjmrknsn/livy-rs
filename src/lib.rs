955b9dc6b6eb493a181584a91a7fb9f7d3c0d0df
Keiji Yoshida <kjmrknsn@gmail.com>
2017-11-28 22:19:26 +0900

    Update v0_3_0

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 //! # livy
 //!
-//! Apache Livy REST API Client
+//! Apache Livy REST API client
 //!
 //! ## Supported Versions of Apache Livy
 //! * 0.3.0
 //! * 0.4.0
 
-/// Apache Livy 0.3.0 REST API Client
+/// Apache Livy 0.3.0 REST API client
 pub mod v0_3_0;
---
+++ src/v0_3_0.rs
@@ -0,0 +1,66 @@
+use std::collections::HashMap;
+
+/// Apache Livy REST API client
+pub struct Client {
+    base_url: String,
+}
+
+impl Client {
+    /// Constructs a new `Client`.
+    ///
+    /// # Examples
+    /// ```
+    /// use livy::v0_3_0::Client;
+    ///
+    /// let client = Client::new("http://example.com:8998/");
+    /// ```
+    pub fn new(base_url: &str) -> Client {
+        Client {
+            base_url: String::from(base_url),
+        }
+    }
+}
+
+/// A session which represents an interactive shell.
+pub struct Session {
+    id: i64,
+    app_id: String,
+    owner: String,
+    proxy_user: String,
+    kind: SessionKind,
+    log: Vec<String>,
+    state: SessionState,
+    appInfo: HashMap<String, String>,
+}
+
+pub enum SessionState {
+    NotStarted,
+    Starting,
+    Idle,
+    Busy,
+    ShuttingDown,
+    Error,
+    Dead,
+    Success,
+}
+
+pub enum SessionKind {
+    Spark,
+    Pyspark,
+    Pyspark3,
+    Sparkr,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn client_new() {
+        let base_url = "http://example.com:8998/";
+
+        let client = Client::new(base_url);
+
+        assert_eq!(base_url, client.base_url);
+    }
+}
